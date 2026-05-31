9e8991e3fba14ff3b09fe1cc799e4bc0a833db4b
patkarmandar <94187044+patkarmandar@users.noreply.github.com>
2026-05-31 16:01:41 +0530

    Fix

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -352,7 +352,7 @@ export default function DashboardPage() {
           </div>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
-            {filtered.map(col => (
+            {filtered.map((col, index) => (
               <div
                 key={col.id}
                 draggable={!search}
